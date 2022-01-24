e30b189ae8b7cd161dd46f853d64a407c069e998
Tony <q10025603@gmail.com>
2022-01-24 21:38:43 +0800

    第一周教材初步整理

---
+++ 網頁整合/Try/ChangePage.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace 網頁整合.Try
+{
+    public partial class ChangePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //string[] s = new string[0];
+            //s[1] = "";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Text = "我選擇一";
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Label1.Text = "我選擇二";
+        }
+    }
+}
